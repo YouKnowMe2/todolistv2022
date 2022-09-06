299bc8fb8cdd08442422a52d40bec9103a99a73f
youknowme2 <highermath80@gmail.com>
2022-09-06 22:13:49 +0600

    what

--- data/database.js
+++ data/database.js
@@ -3,13 +3,13 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let database;
-let MONGODB_URL = 'mongodb+srv://youknowme:youknowme%402019@cluster0.jlfcort.mongodb.net/?retryWrites=true&w=majority';
-
+// let MONGODB_URL = 'mongodb+srv://youknowme:youknowme%402019@cluster0.jlfcort.mongodb.net/?retryWrites=true&w=majority';
+MONGODB_URL = 'http://localhost:27017'
 // if(process.env.MONGODB_URL){
 //     MONGODB_URL = process.env.MONGODB_URL;
 // }
 async function initDb(){
-    const client = await MongoClient.connect(MONGODB_URL);
+    const client = await MongoClient.connect('mongodb://localhost:27017');
     database = client.db('first-api');
 }
 
