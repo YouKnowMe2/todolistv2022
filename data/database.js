e56ebf4a643e30d981d7b88f95c0bcaa9dc0132c
youknowme2 <highermath80@gmail.com>
2022-09-06 21:10:06 +0600

    First COmmit v3

--- data/database.js
+++ data/database.js
@@ -5,9 +5,9 @@ const MongoClient = mongodb.MongoClient;
 let database;
 let MONGODB_URL = 'mongodb+srv://youknowme:youknowme%402019@cluster0.jlfcort.mongodb.net/?retryWrites=true&w=majority';
 
-if(process.env.MONGODB_URL){
-    MONGODB_URL = process.env.MONGODB_URL;
-}
+// if(process.env.MONGODB_URL){
+//     MONGODB_URL = process.env.MONGODB_URL;
+// }
 async function initDb(){
     const client = await MongoClient.connect(MONGODB_URL);
     database = client.db('first-api');
