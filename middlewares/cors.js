13e3a822ea99f3d7ab471b3b23fc47969137a3ca
Md. Abir Chowdhury <40237786+YouKnowMe2@users.noreply.github.com>
2022-09-06 22:48:44 +0600

    Allow from all origin

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,5 @@
 function enableCors(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin','https://capable-medovik-8b144c.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers','Content-Type');
     next();
